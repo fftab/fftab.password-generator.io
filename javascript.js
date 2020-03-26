2a2a341034e73509084d8055e7b594ab16846d99
Florencia Tabarez <fftabarez97@gmail.com>
2020-03-25 20:02:30 -0600

    Refactored code, got rid of use of strings to generate password, employed use of arrays and a new random number generator to pull characters from the arrays chosen by user.

--- javascript.js
+++ javascript.js
@@ -17,10 +17,10 @@ let specialCharacters = ['!','@','#','$','%','^','&','*','~','.','/','<','>','?'
 console.log(specialCharacters);
 
 // Number Array
-let numbersCharacters = ['1','2','3','4','5','6','7','8','9','0'];
+let numbers = ['1','2','3','4','5','6','7','8','9','0'];
 
 // Using the console to confirm numbers array works.
-console.log(numbersCharacters);
+console.log(numbers);
 
 // Uppercase Letter Array --- used code from https://stackoverflow.com/questions/29719329/convert-array-into-upper-case
 let upperCaseLetters = String.prototype.toUpperCase.apply(lowerCaseLetters).split(",");
@@ -30,9 +30,6 @@ console.log(upperCaseLetters);
 
 // Initializing passwordLength Variable.
 let passwordLength;
-
-// Initializing passwordGeneratingCharacters;
-let passwordGeneratingCharacters;
 
 // =====================================================================================
 // ***** User's Password Characters and Length Selection *****
@@ -51,15 +48,14 @@ let passwordGeneratingCriteria = [];
 // ===============================================================================
 
 // Declaring the userLacksChoices Function
-function userLacksChoices() {
+function userChoices() {
 
     // Confirm Lowercase Letters - Asking user to confirm inclusion of Lowercase Letters in Password Generating Criterion(a).
     let wantsLowerCaseLetters = confirm("Include lowercase letters in your password? 'OK' for Yes or 'Cancel' for No");
 
     // Adding to passwordGeneratingCriteria Array if confirmed, and alerting the user of choice.
     if (wantsLowerCaseLetters) {
-        let joinedLowerCaseLetters = lowerCaseLetters.join('');
-        passwordGeneratingCriteria.push(joinedLowerCaseLetters);
+        passwordGeneratingCriteria.push(lowerCaseLetters);
         alert("Your password will include lowercase letters.");
     }
     else { 
@@ -71,8 +67,7 @@ function userLacksChoices() {
 
     // Adding to passwordGeneratingCriteria Array if confirmed, and alerting the user of choice.
     if (wantsUpperCaseLetters) {
-        let joinedUpperCaseLetters = upperCaseLetters.join('');
-        passwordGeneratingCriteria.push(joinedUpperCaseLetters);
+        passwordGeneratingCriteria.push(upperCaseLetters);
         alert("Your password will include uppercase letters.");
     }
     else { 
@@ -84,8 +79,7 @@ function userLacksChoices() {
 
     // Adding to passwordGeneratingCriteria Array if confirmed, and alerting the user of choice.
     if (wantsSpecialCharacters) {
-        let joinedSpecialCharacters = specialCharacters.join('');
-        passwordGeneratingCriteria.push(joinedSpecialCharacters);
+        passwordGeneratingCriteria.push(specialCharacters);
         alert("Your password will include special characters.");
     }
     else { 
@@ -97,8 +91,7 @@ function userLacksChoices() {
 
     // Adding to passwordGeneratingCriteria Array if confirmed, and alerting the user of choice.
     if (wantsNumbers) {
-        let joinedNumbers = numbersCharacters.join('');
-        passwordGeneratingCriteria.push(joinedNumbers);
+        passwordGeneratingCriteria.push(numbers);
         alert("Your password will include numbers.");
     }
     else { 
@@ -110,18 +103,15 @@ function userLacksChoices() {
         alert("Must choose at least one character type to generate password.");
     
         // Recursion
-        userLacksChoices();
+        userChoices();
     }
 
-    // Join Selected Array Characters - Joining all of the Characters in the User's Password Generating Criteria
-    passwordGeneratingCharacters = passwordGeneratingCriteria.join('');
-
-    // Using console to check functionality of actionable password generating string.
-    console.log(passwordGeneratingCharacters);
+    // Using console to check functionality of passwordGeneratingCriteria array.
+    console.log(passwordGeneratingCriteria);
 }
 
 //Calling the userLacksChoices Function
-userLacksChoices();
+userChoices();
 
 // ================================================================================
 // ***** Password Length Prompt & Recursion with Conditional *****
@@ -170,12 +160,16 @@ function generatePassword() {
     // For Loop for Number Generation as (initial is 0; break case: initial is less then given pw length; increment initial by one)
     for (i = 0; i < passwordLength; i++) {
 
-        // Generating Random Number
-        let randomNumber = Math.floor(Math.random() * passwordGeneratingCharacters.length);
-        // Generating Random Characters 
-        let randomCharacters = passwordGeneratingCharacters[randomNumber];
-        // Icrementally adding newCharacters until break case described above.
-        randomPassword = randomPassword + randomCharacters;
+        // Generating Random Number for Index of Character Array 
+        let randomArrayNumber = Math.floor(Math.random() * passwordGeneratingCriteria.length);
+        // Generating Random Character Array
+        let randomCharArr = passwordGeneratingCriteria[randomArrayNumber];
+        // Generating Random Number for Index of Character
+        let randomCharacterNumber = Math.floor(Math.random() * randomCharArr.length);
+        // Generating random character from the array
+        let randomCharacter = randomCharArr[randomCharacterNumber];
+        // Incrementally adding newCharacters until break case described above.
+        randomPassword = randomPassword + randomCharacter;
     }
 
     return randomPassword;
@@ -288,3 +282,117 @@ generateBtn.addEventListener("click", writePassword);
 //     alert("Must choose at least one character type to generate password.");
 
 // }
+
+// // if the user wants an array or multiple arrays of character types (switch cases?),
+// // then let randomPassword match at least one character from each of the arrays chosen by the user
+// // else if there is no match
+// switch () {
+
+//     case "Lowercase":
+//         if (wantsLowerCaseLetters) {
+//             randomPassword.match(regexp: joinedLowerCaseLetters)// could use joinedLowerCaseLetters string or lowerCaseLetters array
+//         };
+//     case "Uppercase":
+//         if (wantsUpperCaseLetters) {
+//             randomPassword.match() // could use joinedUpperCaseLetters string or upperCaseLetters array;
+//         };
+//     case "Spec":
+//         if (wantsSpecialCharacters) {
+//             randomPassword.match() // could use joinedSpecialCharacters string or specialCharacters array;
+//         };
+//     case "Numbers":
+//         if (wantsNumbers) {
+//             randomPassword.match() // could use joinedNumbers string or numbers array;
+//         };
+    
+// }
+
+// // Declaring the userLacksChoices Function
+// function userChoices() {
+
+//     // Confirm Lowercase Letters - Asking user to confirm inclusion of Lowercase Letters in Password Generating Criterion(a).
+//     let wantsLowerCaseLetters = confirm("Include lowercase letters in your password? 'OK' for Yes or 'Cancel' for No");
+
+//     // Adding to passwordGeneratingCriteria Array if confirmed, and alerting the user of choice.
+//     if (wantsLowerCaseLetters) {
+//         let joinedLowerCaseLetters = lowerCaseLetters.join('');
+//         passwordGeneratingCriteria.push(joinedLowerCaseLetters);
+//         alert("Your password will include lowercase letters.");
+//     }
+//     else { 
+//         alert("Your password will not have lowercase letters.");
+//     }
+
+//     // Confirm Uppercase Letters - Asking user to confirm inclusion of Uppercase Letters in Password Generating Criterion(a).
+//     let wantsUpperCaseLetters = confirm("Include uppercase letters in your password? 'OK' for Yes or 'Cancel' for No");
+
+//     // Adding to passwordGeneratingCriteria Array if confirmed, and alerting the user of choice.
+//     if (wantsUpperCaseLetters) {
+//         let joinedUpperCaseLetters = upperCaseLetters.join('');
+//         passwordGeneratingCriteria.push(joinedUpperCaseLetters);
+//         alert("Your password will include uppercase letters.");
+//     }
+//     else { 
+//         alert("Your password will not have uppercase letters.");
+//     }
+
+//     // Confirm Special Characters - Asking user to confirm inclusion of Special Characters in Password Generating Criterion(a).
+//     let wantsSpecialCharacters = confirm("Include special characters in your password? 'OK' for Yes or 'Cancel' for No");
+
+//     // Adding to passwordGeneratingCriteria Array if confirmed, and alerting the user of choice.
+//     if (wantsSpecialCharacters) {
+//         let joinedSpecialCharacters = specialCharacters.join('');
+//         passwordGeneratingCriteria.push(joinedSpecialCharacters);
+//         alert("Your password will include special characters.");
+//     }
+//     else { 
+//         alert("Your password will not have special characters.");
+//     }
+
+//     // Confirm Numbers - Asking user to confirm inclusion of Numbers in Password Generating Criterion(a).
+//     let wantsNumbers = confirm("Include numbers in your password? 'Ok' for Yes or 'Cancel' for No");
+
+//     // Adding to passwordGeneratingCriteria Array if confirmed, and alerting the user of choice.
+//     if (wantsNumbers) {
+//         let joinedNumbers = numbersCharacters.join('');
+//         passwordGeneratingCriteria.push(joinedNumbers);
+//         alert("Your password will include numbers.");
+//     }
+//     else { 
+//         alert("Your password will not have numbers.");
+//     }
+//     // Conditional Statement Indicating User does not want any of the possible character selections.
+//     if (!wantsLowerCaseLetters && !wantsUpperCaseLetters && !wantsSpecialCharacters && !wantsNumbers) {
+//         // Alerts the user of password generation parameters.
+//         alert("Must choose at least one character type to generate password.");
+    
+//         // Recursion
+//         userChoices();
+//     }
+
+//     // Join Selected Array Characters - Joining all of the Characters in the User's Password Generating Criteria
+//     passwordGeneratingCharacters = passwordGeneratingCriteria.join('');
+
+//     // Using console to check functionality of actionable password generating string.
+//     console.log(passwordGeneratingCharacters);
+// }
+// // Initializing passwordGeneratingCharacters;
+// let passwordGeneratingCharacters;
+
+//  // Join Selected Array Characters - Joining all of the Characters in the User's Password Generating Criteria
+//  passwordGeneratingCharacters = passwordGeneratingCriteria.join('');
+
+// // Using console to check functionality of actionable password generating string.
+// console.log(passwordGeneratingCharacters);
+
+
+ // let passwordGeneratingCriteria = [];
+        // if user wants a character array then push that array into the passwordGeneratingCriteria
+        //
+        //  i
+        //  first: use i to keep track of which character in the password we are generating.
+        //  second: use i to get the index of the array we want to use.
+        //  length = 4
+        //  0, 1, 2, 3, 4, 5, 6, 7
+        //  0, 1, 2, 3, 0, 1, 2, 3
+        //  
